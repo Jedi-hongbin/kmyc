ecb524727efc3b65e8efa4a9bc3614e65bfe8191
Hongbin <2218176087@qq.com>
2022-02-06 09:21:58 +0800

    add

--- src/container/Map/index.tsx
+++ src/container/Map/index.tsx
@@ -2,14 +2,13 @@
  * @Author: hongbin
  * @Date: 2022-02-06 09:15:57
  * @LastEditors: hongbin
- * @LastEditTime: 2022-02-06 09:17:11
+ * @LastEditTime: 2022-02-06 09:21:52
  * @Description: three.js 和 glt模型 朝鲜地图模块
  */
 import { FC, ReactElement } from "react";
 require("../../assets/GLTFLoader"); //向THREE注入glt模型加载器 省去手动daro解析器
 
 interface IProps {}
-console.log(THREE.GLTFLoader);
 
 const Map: FC<IProps> = (): ReactElement => {
   return <canvas></canvas>;
