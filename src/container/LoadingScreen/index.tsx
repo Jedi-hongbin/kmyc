0d01bc2cf9b22fedf80c735c0f65cff1f3effe7c
Hongbin <2218176087@qq.com>
2022-03-14 22:47:52 +0800

    nothing

--- src/container/LoadingScreen/index.tsx
+++ src/container/LoadingScreen/index.tsx
@@ -2,7 +2,7 @@
  * @Author: hongbin
  * @Date: 2022-02-06 15:39:40
  * @LastEditors: hongbin
- * @LastEditTime: 2022-03-10 10:58:27
+ * @LastEditTime: 2022-03-14 22:18:20
  * @Description: 加载数据屏 获取数据后进入页面
  */
 import { FC, ReactElement, useEffect, useState } from "react";
